002cd045c66a5973af3f587e3dc093664caa56cf
HarryA123 <harrykwak123@gmail.com>
2022-10-13 23:24:16 +0900

    feat(HomeHeader): 🎸Custom HomeHeaderDrawer

--- src/components/common/@Layout/HomeLayout/_fragments/HomeHeader/_fragments/HomeHeaderDrawer.tsx
+++ src/components/common/@Layout/HomeLayout/_fragments/HomeHeader/_fragments/HomeHeaderDrawer.tsx
@@ -1,11 +1,18 @@
+import Link from 'next/link';
+
 import { ChakraProps, DrawerProps } from '@chakra-ui/react';
 import {
+  Box,
   Drawer,
   DrawerBody,
   DrawerCloseButton,
   DrawerContent,
   DrawerOverlay,
+  Flex,
+  Text,
 } from '@chakra-ui/react';
+
+import ExitIcon from '@components/common/@Icons/System/Exit';
 
 interface HomeHeaderDrawerProps extends Omit<DrawerProps, 'children'> {
   bodyProps?: ChakraProps;
@@ -16,22 +23,53 @@ const HomeHeaderDrawer = ({
   ...basisProps
 }: HomeHeaderDrawerProps) => {
   return (
-    <Drawer placement="right" size="sm" {...basisProps}>
+    <Drawer placement="left" size="xs" {...basisProps}>
       <DrawerOverlay />
       <DrawerContent>
-        <DrawerBody //
-          {...bodyProps}
-          py="0px"
-          px="16px"
-          position="relative"
-        >
-          <DrawerCloseButton //
-            w="40px"
-            h="40px"
-            top="20px"
-            right={{ base: '16px', md: '80px' }}
-            onClick={basisProps.onClose}
-          />
+        <DrawerBody {...bodyProps} p="0" position="relative">
+          <Box h="80px" zIndex="sticky">
+            <DrawerCloseButton
+              w="40px"
+              h="40px"
+              top="20px"
+              right={{ base: '16px', md: '80px' }}
+              onClick={basisProps.onClose}
+            />
+          </Box>
+          <Text mb="30px" textStyle="textLargeBold" ml="16px">
+            카테고리
+          </Text>
+          <Box borderBottom="1px" borderColor="gray.400">
+            {MENU.map((m, idx) => {
+              return (
+                <Link key={idx} href={m.link}>
+                  <Flex
+                    alignItems="center"
+                    borderTop="1px"
+                    borderColor="gray.400"
+                  >
+                    <Text m="16px" textStyle="lg">
+                      {m.title}
+                    </Text>
+                  </Flex>
+                </Link>
+              );
+            })}
+          </Box>
+          <Link href="/Logout">
+            <Flex
+              ml="16px"
+              h="80px"
+              pos="fixed"
+              zIndex="sticky"
+              bottom="0"
+              gap="5px"
+              alignItems="center"
+            >
+              <ExitIcon boxSize="24px" />
+              <Text textStyle="textLargeBold">로그아웃</Text>
+            </Flex>
+          </Link>
         </DrawerBody>
       </DrawerContent>
     </Drawer>
@@ -39,3 +77,9 @@ const HomeHeaderDrawer = ({
 };
 
 export default HomeHeaderDrawer;
+
+const MENU = [
+  { title: '홈', link: `/` },
+  { title: '상품 보기', link: `/product` },
+  { title: '마이페이지', link: `/my` },
+];
