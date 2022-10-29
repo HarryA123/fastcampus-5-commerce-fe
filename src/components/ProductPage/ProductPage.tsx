23df15cd6b632d4b74a2e45ddd9288d1bad30534
HarryA123 <harrykwak123@gmail.com>
2022-10-29 22:16:29 +0900

    feat: Add ProductCard map

--- src/components/ProductPage/ProductPage.tsx
+++ src/components/ProductPage/ProductPage.tsx
@@ -3,34 +3,49 @@ import React from 'react';
 import {
   Box,
   BoxProps,
-  Circle,
   Flex,
-  IconButton,
-  Image,
-  Text,
 } from '@chakra-ui/react';
+
+import { useGetProductQuery } from '@apis/Product/ProductApi.query';
 
 import ProductCard from './_fragments/ProductCard';
 
 interface ProductPageContentProps extends BoxProps {}
 
 function ProductPageContent({ ...basisProps }: ProductPageContentProps) {
+  console.log(useGetProductQuery());
+  const { data } = useGetProductQuery();
+
   return (
     <Box m="0 auto">
-      <Flex
-        direction="column"
-        // border="1px solid red"
-        // bg="gray.100"
-        mt="120px"
-        gap="30px"
-      >
+      <Flex direction="column" mt="120px" gap="30px">
+        {data?.map((item, idx) => {
+          const tags = item.tag.map((item: any) => {
+            return item.name;
+          });
+          return (
+            <>
+              <ProductCard
+                key={idx}
+                tag={tags}
+                avgRate={item.avgRate}
+                reviewCount={item.reviewCount}
+                description={item.description}
+                capacity={item.capacity}
+                thumbnail={item.thumbnail}
+                name={item.name}
+                price={item.price}
+              />
+            </>
+          );
+        })}
+
+        {/* <ProductCard />
         <ProductCard />
         <ProductCard />
         <ProductCard />
         <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
+        <ProductCard /> */}
       </Flex>
     </Box>
   );
