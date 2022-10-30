4624496eff30c896f95918b45b5e0d55d1778f68
HarryA123 <harrykwak123@gmail.com>
2022-10-30 21:34:45 +0900

    feat: Add Global textStyles Style

--- src/styles/theme/textStyles.ts
+++ src/styles/theme/textStyles.ts
@@ -1,19 +1,38 @@
 const textStyles = {
   sm: {
-    fontSize: ['12px', '10px', '12px'],
-    lineHeight: ['18px', '16px', '18px'],
+    fontSize: '12px',
+    lineHeight: '18px',
+    fontWeight: 'bold',
+  },
+  sub: {
+    fontSize: '12px',
+    lineHeight: '18px',
+    fontWeight: 'regular',
   },
   md: {
-    fontSize: { base: '16px', sm: '14px', md: '15px' },
-    lineHeight: ['28px', '26px', '27px'],
+    fontSize: '16px',
+    lineHeight: '28px',
+    fontWeight: 'regular',
   },
   lg: {
-    fontSize: ['20px', '20px', '26px'],
-    lineHeight: ['29px', '29px', '42px'],
+    fontSize: '16px',
+    lineHeight: '28px',
+    fontWeight: 'bold',
   },
   xl: {
-    fontSize: ['26px', '26px', '35px'],
-    lineHeight: ['38px', '38px', '52px'],
+    fontSize: '26px',
+    lineHeight: '38px',
+    fontWeight: 'bold',
+  },
+  textLarge: {
+    fontSize: '20px',
+    lineHeight: '29px',
+    fontWeight: 'regular',
+  },
+  textLargeBold: {
+    fontSize: '20px',
+    lineHeight: '29px',
+    fontWeight: 'Bold',
   },
 };
 
